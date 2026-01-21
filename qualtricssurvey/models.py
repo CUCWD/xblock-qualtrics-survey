5f10344d9d8c1218c450340331e03d490b1edf5e
Zachary Trabookis <ztraboo@clemson.edu>
2026-01-21 16:41:00 -0500

    fix: Handled KeyError for multiple Score categories when looking for key `SC_8Joxltl97oYWdAa` which represents the `Score` category.

--- qualtricssurvey/models.py
+++ qualtricssurvey/models.py
@@ -973,6 +973,10 @@ class QualtricsSurveyModelMixin(ScorableXBlockMixin, CourseDetailsXBlockMixin, O
                                 # Sometimes we may forget to set a score grading value and `#` will get passed from Qualtrics.
                                 LOGGER.warning(u"Qualtrics - max_score() - Issue with getting raw_possible for – Survey ID ({}) QID ({}) – {}".format(self.get_survey_id(), question["QuestionID"], err))
                                 continue
+                            except KeyError as err:
+                                # Sometimes we may forget to set a score grading value and `#` will get passed from Qualtrics.
+                                LOGGER.warning(u"Qualtrics – max_score() – Issue with getting raw_possible for – Survey ID ({}) QID ({}) – {}".format(self.get_survey_id(), question["QuestionID"], err))
+                                continue
         else:
             # Awards full points for completing a survey (default)
             raw_possible = (self.weight if self.weight is not None and self.weight > 0 else 0.0)
